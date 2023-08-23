acf44e4ddf6ea5a49c8722f455eb0a3d63afc6c6
Sasha Afanasienka <afanasienka@yandex.by>
2023-08-23 23:48:08 +0300

    feature: add workable ws server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const io = require('socket.io')(http);
 const express = require('express');
 const app = express();
 const port = 8000; // Порт, на котором будет запущен сервер
+const WebSocket = require('ws');
 
 app.get('/', (req, res) => {
   res.send({text: 'Hello world'});
@@ -12,14 +13,35 @@ app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
 });
 
-const server = http.createServer();
-io.listen(server);
+// const Deck = require('./modules/deck');
+// const Player = require('./modules/player');
+// const getRandomId = require('./utils/getRandomId');
+// const getRandomInteger = require('./utils/getRandomInteger');
 
-server.listen(3000, () => {
-  console.log('WebSocket server started on port 3000');
-});
+class Game {
+	constructor() {
+		this.server = new WebSocket.Server({
+			port: 8079,
+		});
+		this.deck = undefined;
+		this.queue = [];
+		this.data = {
+			players: [],
+			onTop: null,
+			reverse: false,
+		};
+		this.config = {
+			maxPLayers: 4,
+		};
+	}
 
-// Обработка веб-сокет соединений
-io.on('connection', (socket) => {
-  console.log('connection')
-});
+	init() {
+		this.server.on('connection', (socket) => {
+      socket.send('connected');
+      console.log('conneected');
+		});
+	}
+}
+
+const game = new Game();
+game.init();
