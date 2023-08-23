b48480667928196ce09c05037d71ad332f8ec447
Sasha Afanasienka <afanasienka@yandex.by>
2023-08-23 23:26:02 +0300

    feature: add websocket server

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const http = require('http');
+const io = require('socket.io')(http);
 const express = require('express');
 const app = express();
 const port = 8000; // Порт, на котором будет запущен сервер
@@ -8,4 +10,16 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
+});
+
+const server = http.createServer();
+io.listen(server);
+
+server.listen(3000, () => {
+  console.log('WebSocket server started on port 3000');
+});
+
+// Обработка веб-сокет соединений
+io.on('connection', (socket) => {
+  console.log('connection')
 });
